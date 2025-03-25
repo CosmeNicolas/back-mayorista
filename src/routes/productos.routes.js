a6a4db1491450c0cf8db2a67973ff02430eff30b
CosmeNicolas <nicomas7590@gmail.com>
2025-03-24 21:09:14 -0300

    le di formato a las rutas de los productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { check } = require('express-validator')
+const { check } = require("express-validator");
 const {
   crearProducto,
   mostrarProductos,
@@ -19,26 +19,41 @@ const {
 const multer = require("../middleware/multer");
 const auth = require("../middleware/auth");
 
-
 const router = Router();
-
 
 router.get("/", mostrarProductos);
 
-router.get("/:idProducto",/* auth('usuario'), */ mostrarUnProducto);
+router.get("/:idProducto", /* auth('usuario'), */ mostrarUnProducto);
 
-router.get("/obtenerFavoritoCarrito/",/* auth('usuario'), */ obtenerFavoritoUsuario)
-router.get("/obtenerProdCart",/* auth('usuario'), */obtenerProductoCarritoUsuario)
+router.get(
+  "/obtenerFavoritoCarrito/",
+  /* auth('usuario'), */ obtenerFavoritoUsuario
+);
+router.get(
+  "/obtenerProdCart",
+  /* auth('usuario'), */ obtenerProductoCarritoUsuario
+);
 
 /* actualizaciones */
-router.put("/:id", [
-  check("nombreProducto", "El PRODUCTO no debe estar vacio").not().isEmpty(),
-  check("precio", "Campo PRECIO vacio").not().isEmpty(),
-  check("descripcion", "campo DESCRIPCION vacio ").isLength({ min: 8, max: 40 }),
-],auth('admin'),actualizarProducto);
+router.put(
+  "/:id",
+  [
+    check("nombreProducto", "El PRODUCTO no debe estar vacio").not().isEmpty(),
+    check("precio", "Campo PRECIO vacio").not().isEmpty(),
+    check("descripcion", "campo DESCRIPCION vacio ").isLength({
+      min: 8,
+      max: 40,
+    }),
+  ],
+  auth("admin"),
+  actualizarProducto
+);
 
-router.put('/habilitar/:idProducto'/* ,auth('admin') */,habilitarUnProducto)
-router.put('/deshabilitar/:idProducto'/* ,auth('admin') */,deshabilitarUnProducto)
+router.put("/habilitar/:idProducto" /* ,auth('admin') */, habilitarUnProducto);
+router.put(
+  "/deshabilitar/:idProducto" /* ,auth('admin') */,
+  deshabilitarUnProducto
+);
 
 router.post(
   "/",
@@ -52,16 +67,33 @@ router.post(
   ],
   crearProducto
 );
-router.post('/agregarProdFav/:idProducto',auth('usuario'),agregarProductoAFavoritos )
-router.post('/agregarProdCart/:idProducto',auth('usuario'),agregarProductoAlCarrito )
+router.post(
+  "/agregarProdFav/:idProducto",
+  auth("usuario"),
+  agregarProductoAFavoritos
+);
+router.post(
+  "/agregarProdCart/:idProducto",
+  auth("usuario"),
+  agregarProductoAlCarrito
+);
 
-
-router.delete('/borrarProdFav/:idProducto',/* auth('usuario'), */borrarProductoDeFavoritos)
-router.delete('/borrarProdCart/:idProducto',/* auth('usuario'), */borrarProductoCarrito)
-router.delete("/:idProducto",/* auth('admin'), */ eliminarProducto);
+router.delete(
+  "/borrarProdFav/:idProducto",
+  /* auth('usuario'), */ borrarProductoDeFavoritos
+);
+router.delete(
+  "/borrarProdCart/:idProducto",
+  /* auth('usuario'), */ borrarProductoCarrito
+);
+router.delete("/:idProducto", /* auth('admin'), */ eliminarProducto);
 
 /* rutas imagenes al producto / .single para una fotos, .array muchas fotos */
-router.post('/agregarImagen/:id',multer.single('imagen'),agregarImagenProducto)
+router.post(
+  "/agregarImagen/:id",
+  multer.single("imagen"),
+  agregarImagenProducto
+);
 
 /* exportamos  */
 module.exports = router;

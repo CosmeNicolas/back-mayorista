a65fd948d49fa7cdf1ab91cdb72cc4db16e79704
cosmeNicolas <nicomas7590@gmail.com>
2025-05-04 20:12:20 -0300

    cambie el esquema de productos para agregar talles y categorias

--- src/models/productos.shema.js
+++ src/models/productos.shema.js
@@ -1,32 +1,56 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const ProductoSchema = new Schema({
   nombreProducto: {
     type: String,
     required: true,
     trim: true,
-    min: [5,'Minimos permitido 5 caracteres'],
-    max: [50, 'Maximo permitido 50 caracteres'],
+    minlength: [5, "Mínimo permitido 5 caracteres"],
+    maxlength: [50, "Máximo permitido 50 caracteres"]
   },
-  precio:{
-    type:Number,
+  precio: {
+    type: Number,
     required: true,
   },
-  descripcion:{
-    type:String,
-    required: true,
-    trim: true
-  },
-  imagen:{
+  descripcion: {
     type: String,
-    default: ''
+    required: true,
+    trim: true,
+  },
+  imagen: {
+    type: String,
+    default: '',
   },
   bloqueado: {
     type: Boolean,
-    default: false
+    default: false,
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: [
+      'COLECCION OTOÑO INVIERNO 2025',
+      'CAMPERAS Y ABRIGOS',
+      'CHALECOS',
+      'POLLERAS Y SHORT',
+      'SASTRERIA',
+      'PANTALON',
+      'CAMISAS Y REMERAS'
+    ]
+  },
+  stockPorTalle: {
+    type: Map,
+    of: Number,
+    default: {
+      S: 0,
+      M: 0,
+      L: 0,
+      XL: 0,
+      XXL: 0
+    }
   }
-})
+});
 
-const ProductoModel = model('productos',ProductoSchema)
+const ProductoModel = model("productos", ProductoSchema);
 
-module.exports = ProductoModel
+module.exports = ProductoModel;
